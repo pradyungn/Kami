bb5eb23c3bac294b0e61d19ef8196bdfbc06567f
Soham Bhattacharya <sohamb117@gmail.com>
2020-07-17 23:02:26 -0700

    Working on OCR API

--- backend/ocr/main.py
+++ backend/ocr/main.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 from flask import Flask
 from PIL import Image
-import pytesseract
+import pytesseract as pyocr
+import io
+from flask import request
 app = Flask(__name__)
+
+@app.route("/ocr", methods = ['POST','GET'])
+def ocr():
+    encode_png = request.args.get("encode", type = str)
+    return(pyocr.image_to_string(Image.open(io.BytesIO(str.decode('base64')))))
+
 
 
