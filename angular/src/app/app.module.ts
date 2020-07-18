c4b8424aeb1e086176f2aeb064927f0688984567
Kyle <sdbagel@gmail.com>
2020-07-17 21:38:11 -0700

    Materialize buttons, form, login, home

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import '../polyfills';
 
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
@@ -27,6 +28,7 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
     CoreModule,
--- angular/src/app/home/home.module.ts
+++ angular/src/app/home/home.module.ts
@@ -7,8 +7,17 @@ import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './login/login.component';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
 @NgModule({
   declarations: [HomeComponent, LoginComponent],
-  imports: [CommonModule, SharedModule, HomeRoutingModule]
+  imports: [
+    CommonModule, 
+    SharedModule, 
+    HomeRoutingModule, 
+    MatButtonModule,
+    MatInputModule
+  ]
 })
 export class HomeModule {}
