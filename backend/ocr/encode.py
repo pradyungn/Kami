6c752303b25534e7635576203e137e4e86408ee7
Soham Bhattacharya <sohamb117@gmail.com>
2020-07-18 13:00:08 -0700

    Fixed Everything, OCR Complete?

--- backend/ocr/encode.py
+++
@@ -1,3 +0,0 @@
-import base64
-with open("test.png", "rb") as f:
-  print(base64.urlsafe_b64encode(f.read()).decode("ascii"))
--- backend/ocr/tess.py
+++
@@ -1,8 +0,0 @@
-import pytesseract
-from PIL import Image,ImageFile
-import io, base64
-encode_string = input() + '==='
-decode_string = base64.urlsafe_b64decode(encode_string)
-image = Image.open(io.BytesIO(decode_string))
-image.show()
-print(pytesseract.image_to_string(image))
