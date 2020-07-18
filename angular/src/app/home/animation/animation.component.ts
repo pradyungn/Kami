299994bd6a609a5c9f7a2b35b9f72b9e0f0fc456
Kyle <sdbagel@gmail.com>
2020-07-18 15:20:03 -0700

    Animation mock

--- angular/src/app/home/animation/animation.component.ts
+++ angular/src/app/home/animation/animation.component.ts
@@ -10,6 +10,25 @@ export class AnimationComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      var button = document.getElementById("animation-compress-button");
+
+      document.getElementById("animation-progress-bar").style.opacity = "1";
+      document.getElementById("animation-content").style.opacity = "0";
+
+      button.style.backgroundColor = "var(--dark-blue)";
+      button.style.color = "var(--white)";
+
+      setTimeout(() => {
+        document.getElementById("animation-progress-bar").style.opacity = "0";
+        button.style.backgroundColor = "";
+        button.style.color = "";
+
+        document.getElementById("animation-content").style.display = "none";
+        document.getElementById("animation-end").style.display = "flex";
+        
+      }, 1500);
+    }, 2500);
   }
 
 }
