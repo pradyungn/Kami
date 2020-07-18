36eb1998fa831ffa7a2e92af6dce30bf4d8ba828
Soham Bhattacharya <sohamb117@gmail.com>
2020-07-17 23:28:48 -0700

    Tesseract error, will debug

--- backend/ocr/main.py
+++ backend/ocr/main.py
@@ -4,12 +4,15 @@ from PIL import Image
 import pytesseract as pyocr
 import io
 from flask import request
+import base64
 app = Flask(__name__)
 
 @app.route("/ocr", methods = ['POST','GET'])
 def ocr():
     encode_png = request.args.get("encode", type = str)
-    return(pyocr.image_to_string(Image.open(io.BytesIO(str.decode('base64')))))
+    encode_string = base64.b64decode(encode_png)
+    #return(encode_string)
+    return(pyocr.image_to_string(Image.open(io.BytesIO(encode_string))))
 
 
 
