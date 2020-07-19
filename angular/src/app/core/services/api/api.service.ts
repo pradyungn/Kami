9e2a604b89d27056fb3292294efc10256d4cb9d3
Kyle <sdbagel@gmail.com>
2020-07-19 00:26:58 -0700

    API service for NLP and OCR

--- angular/src/app/core/services/api/api.service.ts
+++ angular/src/app/core/services/api/api.service.ts
@@ -5,6 +5,39 @@ import { Injectable } from '@angular/core';
 })
 export class APIService {
 
-  constructor() {
+  constructor() { }
+
+  // long text -> callback(summary text)
+  getNLP(input: string, callback: (output: string) => void) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onload = () => {
+      // process response
+      if (xhr.status == 200)
+        callback(JSON.parse(xhr.response).output);
+      else
+        console.error('Error!');
+    };
+
+    xhr.open("POST", "api.kamiapp.ml/nlp", true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.send(`{"input": "${input}"}`);
+  }
+
+  // base64 image -> callback(long text)
+  getOCR(input: string, callback: (output: string) => void) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onload = () => {
+      // process response
+      if (xhr.status == 200)
+        callback(JSON.parse(xhr.response).output);
+      else
+        console.error('Error!');
+    };
+
+    xhr.open("POST", "api.kamiapp.ml/ocr", true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.send(`{"input": "${input}"}`);
   }
 }
