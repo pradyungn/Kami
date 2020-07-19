10a94d4e0306ff9e08fc53b7f086f06e09a14674
Kyle <sdbagel@gmail.com>
2020-07-18 23:11:26 -0700

    Delete notes

---
+++ angular/src/app/note/delete/delete.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete',
+  templateUrl: './delete.component.html'
+})
+export class DeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- angular/src/app/note/note.component.ts
+++ angular/src/app/note/note.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
+import { MatDialog } from '@angular/material/dialog';
+import { DeleteComponent } from './delete/delete.component';
+
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
@@ -13,17 +16,32 @@ import { Kami$Note } from '../shared/schemas/note.schema';
   styleUrls: ['./note.component.scss']
 })
 export class NoteComponent implements OnInit {
+  db: firebase.firestore.Firestore;
   note: Kami$Note;
+  noteId: string;
 
   constructor(private router: Router,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private dialog: MatDialog) { }
 
   ngOnInit(): void {
     let id = this.route.snapshot.paramMap.get("id");
-    console.log(id);
+    this.noteId = id;
 
-    var db = firebase.firestore();
-    db.collection(firebase.auth().currentUser.uid).doc(id).get().then(data => this.note = <Kami$Note><unknown>data.data());
+    this.db = firebase.firestore();
+    this.db.collection(firebase.auth().currentUser.uid).doc(id).get().then(data => {
+      this.note = <Kami$Note><unknown>data.data(); 
+    });
   }
 
+  openDeleteDialog(): void {
+    const ref = this.dialog.open(DeleteComponent);
+
+    ref.afterClosed().subscribe(result => {
+      if (result) {
+        this.db.collection(firebase.auth().currentUser.uid).doc(this.noteId).delete();
+        this.router.navigateByUrl("/dashboard");
+      }
+    });
+  }
 }
