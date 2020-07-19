6e80ccf9ba32cb3e1ed0ded011630bd64be4e859
Kyle <sdbagel@gmail.com>
2020-07-19 00:54:07 -0700

    NLP text paste API integration

--- angular/src/app/shared/components/account-header/text/text.component.ts
+++ angular/src/app/shared/components/account-header/text/text.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
+import { APIService } from '../../../../core/services/';
+
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
 @Component({
   selector: 'dialog-createfrom-text',
   templateUrl: './text.component.html',
@@ -9,9 +15,23 @@ export class TextComponent implements OnInit {
   @Input() shown: boolean;
   @Output() closeEvent = new EventEmitter();
 
-  constructor() { }
+  title: string = "";
+  text: string = "";
+
+  constructor(private api: APIService) { }
 
   ngOnInit(): void {
+  }
+
+  submitText(): void {
+    this.api.getNLP(this.text, (output) => {
+      var db = firebase.firestore();
+      db.collection(firebase.auth().currentUser.uid).add({
+        "title": this.title,
+        "text": this.text,
+        "summary": output
+      });
+    });
   }
 
   closeDialog() {
